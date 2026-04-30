5de4b40f8961988152dafb6574c13c9bf9408834
Gabriel Teixeira <louistx@live.com>
2026-04-29 22:21:03 -0300

    reestruturão do diretorio, sem adição de código

--- src/TechChallenge.Api/Program.cs
+++ src/TechChallenge.Api/Program.cs
@@ -12,6 +12,8 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
+
+app.UseSwaggerUI();
 app.UseHttpsRedirection();
 
 var summaries = new[]
---
+++ tests/TechChallenge.Api.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿namespace TechChallenge.Api.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
