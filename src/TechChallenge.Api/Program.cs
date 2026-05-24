5c129e3ae4a91d853448ff297068b92db04739a2
Brunno de Oliveira <brunno-o@live.com>
2026-05-24 12:24:45 -0300

    Adicionado chamada do Middleware de exceções e pacote Swagger.

--- src/TechChallenge.Api/Program.cs
+++ src/TechChallenge.Api/Program.cs
@@ -1,4 +1,5 @@
 using Scalar.AspNetCore;
+using TechChallenge.Api.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,7 +12,11 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.UseMiddleware<ExceptionMiddleware>();
 
 app.MapOpenApi();
 
