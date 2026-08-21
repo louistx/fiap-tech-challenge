2ec0ff3a8e3d042d1a23b4fe9e434ff6386dfa23
Brunno de Oliveira <brunno-o@live.com>
2026-08-20 21:11:34 -0300

    Correção do serviço de Estoque e injeções de dependência

--- src/TechChallenge.Application/Features/Estoque/AdicionarEstoque/AdicionarEstoqueService.cs
+++ src/TechChallenge.Application/Features/Estoque/AdicionarEstoque/AdicionarEstoqueService.cs
@@ -6,11 +6,13 @@ namespace TechChallenge.Application.Features.Estoque.AdicionarEstoque;
 public class AdicionarEstoqueService
 {
     private readonly IEstoqueRepository _estoqueRepository;
+    private readonly IProdutoRepository _produtoRepository;
     private readonly IValidator<AdicionarEstoqueCommand> _validator;
 
-    public AdicionarEstoqueService(IEstoqueRepository estoqueRepository, IValidator<AdicionarEstoqueCommand> validator)
+    public AdicionarEstoqueService(IEstoqueRepository estoqueRepository, IProdutoRepository produtoRepository, IValidator<AdicionarEstoqueCommand> validator)
     {
         _estoqueRepository = estoqueRepository;
+        _produtoRepository = produtoRepository;
         _validator = validator;
     }
 
@@ -18,13 +20,21 @@ public class AdicionarEstoqueService
     {
         _validator.ValidateAndThrow(command);
 
-        var estoque = _estoqueRepository.GetByIdAsync(command.ProdutoId).GetAwaiter().GetResult();
+        var produto = _produtoRepository.GetByIdAsync(command.ProdutoId).GetAwaiter().GetResult();
+
+        if (produto is null)
+            throw new KeyNotFoundException($"Produto {command.ProdutoId} não encontrado");
+
+        var estoque = _estoqueRepository.GetByIdProdutoAsync(command.ProdutoId).GetAwaiter().GetResult();
+        
         if (estoque is null)
-            throw new KeyNotFoundException($"Estoque com Id {command.ProdutoId} não encontrado.");
+            _estoqueRepository.AddAsync(new Domain.Entities.Estoque(Guid.NewGuid(), command.ProdutoId, command.Quantidade));
+        else
+        {
+            estoque.AtualizarQuantidade(estoque.Quantidade + command.Quantidade);
+            _estoqueRepository.UpdateAsync(estoque).GetAwaiter().GetResult();
+        }
 
-        estoque.AtualizarQuantidade(estoque.Quantidade + command.Quantidade);
-
-        _estoqueRepository.UpdateAsync(estoque).GetAwaiter().GetResult();
         return true;
     }
 }
